6bbb9529d05d056f4f8c15610d389e43bd8d4661
AKAKPO ELONM LUC <elonmlucakakpo@gmail.com>
2025-08-29 09:48:36 +0100

    making header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="w-full h-[100px] bg-blue-300">
+            <div className="w-[144px] h-[32px] top-[34px] left-[120px]">
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
