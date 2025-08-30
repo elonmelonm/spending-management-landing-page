23b383317eb4223338dd9efbaaf2be2887f6dc27
AKAKPO ELONM LUC <elonmlucakakpo@gmail.com>
2025-08-30 19:50:36 +0100

    fix second benefits section

--- src/components/BenefitsSecondSection.jsx
+++ src/components/BenefitsSecondSection.jsx
@@ -6,81 +6,83 @@ import Content from '../assets/Content.png';
 
 const BenefitsSecondSection = () => {
     return (
-        <section className="w-[1440px] h-[903px] relative bg-white flex flex-col">
-            <div className="flex flex-col w-[712px] h-[186px] absolute items-center justify-center top-[120px] left-[364px] gap-6">
-                <div className="w-[452px] h-[102px] flex flex-col items-center justify-center  gap-3">
-                    <span className="w-[183px] h-[30px] text-primary-500 font-semibold text-[20px]">
-                        WHY USE SPEND.IN
-                    </span>
-                    <span className="w-[452px] h-[60px] text-secondary-900 font-bold text-[40px] tracking-[-0.03em]">
-                        Easy, Simple, Affordable
-                    </span>
-                </div>
-                <div className="w-[712px] h-[60px] text-secondary-400 text-center">
-                    Our platform helps your business in managing expenses. These are some of the reasons why you should use our platform in managing business finances.
-                </div>
-            </div>
-
-            <div className="flex flex-col w-[497px] h-[376px] absolute justify-center top-[383px] left-[120px] gap-11">
-                <div className="flex w-[497px] h-[96px] gap-8">
-                    <span>
-                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="50" height="50" rx="12" fill="#7C5CFC"/>
-                            <path d="M18 28C17.06 28 16.19 28.33 15.5 28.88C14.58 29.61 14 30.74 14 32C14 34.21 15.79 36 18 36C19.01 36 19.93 35.62 20.64 35C21.47 34.27 22 33.2 22 32C22 29.79 20.21 28 18 28ZM19 32.25C19 32.51 18.86 32.76 18.64 32.89L17.39 33.64C17.27 33.72 17.13 33.75 17 33.75C16.75 33.75 16.5 33.62 16.36 33.39C16.15 33.03 16.26 32.57 16.62 32.36L17.51 31.83V30.75C17.5 30.34 17.84 30 18.25 30C18.66 30 19 30.34 19 30.75V32.25Z" fill="white"/>
-                            <path d="M27.85 16.95V20.75H26.35V16.95C26.35 16.68 26.11 16.55 25.95 16.55C25.9 16.55 25.85 16.56 25.8 16.58L17.87 19.57C17.34 19.77 17 20.27 17 20.84V21.51C16.09 22.19 15.5 23.28 15.5 24.51V20.84C15.5 19.65 16.23 18.59 17.34 18.17L25.28 15.17C25.5 15.09 25.73 15.05 25.95 15.05C26.95 15.05 27.85 15.86 27.85 16.95Z" fill="white"/>
-                            <path d="M34.5001 27.5V28.5C34.5001 28.77 34.2901 28.99 34.0101 29H32.5501C32.0201 29 31.5401 28.61 31.5001 28.09C31.4701 27.78 31.5901 27.49 31.7901 27.29C31.9701 27.1 32.2201 27 32.4901 27H34.0001C34.2901 27.01 34.5001 27.23 34.5001 27.5Z" fill="white"/>
-                            <path d="M32.48 25.95H33.5C34.05 25.95 34.5 25.5 34.5 24.95V24.51C34.5 22.44 32.81 20.75 30.74 20.75H19.26C18.41 20.75 17.63 21.03 17 21.51C16.09 22.19 15.5 23.28 15.5 24.51V26.29C15.5 26.67 15.9 26.91 16.26 26.79C16.82 26.6 17.41 26.5 18 26.5C21.03 26.5 23.5 28.97 23.5 32C23.5 32.72 23.31 33.51 23.01 34.21C22.85 34.57 23.1 35 23.49 35H30.74C32.81 35 34.5 33.31 34.5 31.24V31.05C34.5 30.5 34.05 30.05 33.5 30.05H32.63C31.67 30.05 30.75 29.46 30.5 28.53C30.3 27.77 30.54 27.03 31.04 26.55C31.41 26.17 31.92 25.95 32.48 25.95ZM27 25.75H22C21.59 25.75 21.25 25.41 21.25 25C21.25 24.59 21.59 24.25 22 24.25H27C27.41 24.25 27.75 24.59 27.75 25C27.75 25.41 27.41 25.75 27 25.75Z" fill="white"/>
-                        </svg>
-                    </span>
-                    <div className="flex flex-col w-[415px] h-[96px] gap-3">
-                        <span className="w-[300px] h-[36px] text-secondary-900 text-[24px] font-semibold">
-                            Automatic Invoice Payment
+        <section className="w-full h-[903px] p-[120px] relative items-center bg-white flex flex-col">
+            <div className="flex flex-col items-center gap-16">
+                <div className="flex flex-col w-[712px] h-[186px] items-center justify-center gap-6">
+                    <div className="w-[550px] h-[102px] flex flex-col items-center justify-center  gap-3">
+                        <span className="w-full h-[30px] text-center text-primary-500 font-semibold text-[20px]">
+                            WHY USE SPEND.IN
                         </span>
-                        <span className="w-[415px] h-[48px] text-secondary-400 text-[16px]">
-                            Automatic payments help you to arrange payments on a certain date without doing it manually again.
+                        <span className="w-full h-[60px] text-secondary-900 font-bold text-center text-[40px] tracking-[-0.03em]">
+                            Easy, Simple, Affordable
                         </span>
                     </div>
-                </div>
-                <div className="flex w-[497px] h-[96px] gap-8">
-                    <span>
-                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="50" height="50" rx="12" fill="#7C5CFC"/>
-                            <path d="M29 15H21C17.5 15 16 17 16 20V30C16 33 17.5 35 21 35H29C32.5 35 34 33 34 30V20C34 17 32.5 15 29 15ZM21 25.25H25C25.41 25.25 25.75 25.59 25.75 26C25.75 26.41 25.41 26.75 25 26.75H21C20.59 26.75 20.25 26.41 20.25 26C20.25 25.59 20.59 25.25 21 25.25ZM29 30.75H21C20.59 30.75 20.25 30.41 20.25 30C20.25 29.59 20.59 29.25 21 29.25H29C29.41 29.25 29.75 29.59 29.75 30C29.75 30.41 29.41 30.75 29 30.75ZM31.5 22.25H29.5C27.98 22.25 26.75 21.02 26.75 19.5V17.5C26.75 17.09 27.09 16.75 27.5 16.75C27.91 16.75 28.25 17.09 28.25 17.5V19.5C28.25 20.19 28.81 20.75 29.5 20.75H31.5C31.91 20.75 32.25 21.09 32.25 21.5C32.25 21.91 31.91 22.25 31.5 22.25Z" fill="white"/>
-                        </svg>
-
-                    </span>
-                    <div className="flex flex-col w-[415px] h-[96px] gap-3">
-                        <span className="w-[240px] h-[36px] text-secondary-900 text-[24px] font-semibold">
-                            Clear payment history
-                        </span>
-                        <span className="w-[415px] h-[48px] text-secondary-400 text-[16px]">
-                            Clear payment history helps you to track your business expenses on specific dates.
-                        </span>
+                    <div className="w-[712px] h-[60px] text-secondary-400 text-center">
+                        Our platform helps your business in managing expenses. These are some of the reasons why you should use our platform in managing business finances.
                     </div>
                 </div>
-                <div className="flex w-[497px] h-[96px] gap-8">
-                    <span>
-                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="50" height="50" rx="12" fill="#7C5CFC"/>
-                            <path d="M28.22 19.55H18.78C18.5 19.55 18.24 19.56 18 19.57C15.63 19.71 15 20.58 15 23.28V23.86C15 24.41 15.45 24.86 16 24.86H31C31.55 24.86 32 24.41 32 23.86V23.28C32 20.3 31.24 19.55 28.22 19.55Z" fill="white"/>
-                            <path d="M16 26.3601C15.45 26.3601 15 26.8101 15 27.3601V30.2701C15 33.2501 15.76 34.0001 18.78 34.0001H28.22C31.19 34.0001 31.97 33.2801 32 30.4301V27.3601C32 26.8101 31.55 26.3601 31 26.3601H16ZM19.96 31.5601H18.25C17.84 31.5601 17.5 31.2201 17.5 30.8101C17.5 30.4001 17.84 30.0601 18.25 30.0601H19.97C20.38 30.0601 20.72 30.4001 20.72 30.8101C20.72 31.2201 20.38 31.5601 19.96 31.5601ZM25.55 31.5601H22.11C21.7 31.5601 21.36 31.2201 21.36 30.8101C21.36 30.4001 21.7 30.0601 22.11 30.0601H25.55C25.96 30.0601 26.3 30.4001 26.3 30.8101C26.3 31.2201 25.97 31.5601 25.55 31.5601Z" fill="white"/>
-                            <path d="M35 26.3301V21.0901C35 17.9601 33.21 16.6001 30.51 16.6001H21.58C20.82 16.6001 20.14 16.7101 19.54 16.9401C19.07 17.1101 18.65 17.3601 18.31 17.6901C18.13 17.8601 18.27 18.1401 18.53 18.1401H29.4C31.65 18.1401 33.47 19.9601 33.47 22.2101V29.3801C33.47 29.6301 33.74 29.7701 33.92 29.5901C34.61 28.8601 35 27.7901 35 26.3301Z" fill="white"/>
-                        </svg>
+                <div className="flex flex-row items-center gap-[95px]">
+                    <div className="flex flex-col w-[497px] h-[376px] justify-center gap-11">
+                        <div className="flex w-[497px] h-[96px] gap-8">
+                            <span>
+                                <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect width="50" height="50" rx="12" fill="#7C5CFC" />
+                                    <path d="M18 28C17.06 28 16.19 28.33 15.5 28.88C14.58 29.61 14 30.74 14 32C14 34.21 15.79 36 18 36C19.01 36 19.93 35.62 20.64 35C21.47 34.27 22 33.2 22 32C22 29.79 20.21 28 18 28ZM19 32.25C19 32.51 18.86 32.76 18.64 32.89L17.39 33.64C17.27 33.72 17.13 33.75 17 33.75C16.75 33.75 16.5 33.62 16.36 33.39C16.15 33.03 16.26 32.57 16.62 32.36L17.51 31.83V30.75C17.5 30.34 17.84 30 18.25 30C18.66 30 19 30.34 19 30.75V32.25Z" fill="white" />
+                                    <path d="M27.85 16.95V20.75H26.35V16.95C26.35 16.68 26.11 16.55 25.95 16.55C25.9 16.55 25.85 16.56 25.8 16.58L17.87 19.57C17.34 19.77 17 20.27 17 20.84V21.51C16.09 22.19 15.5 23.28 15.5 24.51V20.84C15.5 19.65 16.23 18.59 17.34 18.17L25.28 15.17C25.5 15.09 25.73 15.05 25.95 15.05C26.95 15.05 27.85 15.86 27.85 16.95Z" fill="white" />
+                                    <path d="M34.5001 27.5V28.5C34.5001 28.77 34.2901 28.99 34.0101 29H32.5501C32.0201 29 31.5401 28.61 31.5001 28.09C31.4701 27.78 31.5901 27.49 31.7901 27.29C31.9701 27.1 32.2201 27 32.4901 27H34.0001C34.2901 27.01 34.5001 27.23 34.5001 27.5Z" fill="white" />
+                                    <path d="M32.48 25.95H33.5C34.05 25.95 34.5 25.5 34.5 24.95V24.51C34.5 22.44 32.81 20.75 30.74 20.75H19.26C18.41 20.75 17.63 21.03 17 21.51C16.09 22.19 15.5 23.28 15.5 24.51V26.29C15.5 26.67 15.9 26.91 16.26 26.79C16.82 26.6 17.41 26.5 18 26.5C21.03 26.5 23.5 28.97 23.5 32C23.5 32.72 23.31 33.51 23.01 34.21C22.85 34.57 23.1 35 23.49 35H30.74C32.81 35 34.5 33.31 34.5 31.24V31.05C34.5 30.5 34.05 30.05 33.5 30.05H32.63C31.67 30.05 30.75 29.46 30.5 28.53C30.3 27.77 30.54 27.03 31.04 26.55C31.41 26.17 31.92 25.95 32.48 25.95ZM27 25.75H22C21.59 25.75 21.25 25.41 21.25 25C21.25 24.59 21.59 24.25 22 24.25H27C27.41 24.25 27.75 24.59 27.75 25C27.75 25.41 27.41 25.75 27 25.75Z" fill="white" />
+                                </svg>
+                            </span>
+                            <div className="flex flex-col w-[415px] h-[96px] gap-3">
+                                <span className="w-full h-[36px] text-secondary-900 text-[24px] font-semibold">
+                                    Automatic Invoice Payment
+                                </span>
+                                <span className="w-[415px] h-[48px] text-secondary-400 text-[16px]">
+                                    Automatic payments help you to arrange payments on a certain date without doing it manually again.
+                                </span>
+                            </div>
+                        </div>
+                        <div className="flex w-[497px] h-[96px] gap-8">
+                            <span>
+                                <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect width="50" height="50" rx="12" fill="#7C5CFC" />
+                                    <path d="M29 15H21C17.5 15 16 17 16 20V30C16 33 17.5 35 21 35H29C32.5 35 34 33 34 30V20C34 17 32.5 15 29 15ZM21 25.25H25C25.41 25.25 25.75 25.59 25.75 26C25.75 26.41 25.41 26.75 25 26.75H21C20.59 26.75 20.25 26.41 20.25 26C20.25 25.59 20.59 25.25 21 25.25ZM29 30.75H21C20.59 30.75 20.25 30.41 20.25 30C20.25 29.59 20.59 29.25 21 29.25H29C29.41 29.25 29.75 29.59 29.75 30C29.75 30.41 29.41 30.75 29 30.75ZM31.5 22.25H29.5C27.98 22.25 26.75 21.02 26.75 19.5V17.5C26.75 17.09 27.09 16.75 27.5 16.75C27.91 16.75 28.25 17.09 28.25 17.5V19.5C28.25 20.19 28.81 20.75 29.5 20.75H31.5C31.91 20.75 32.25 21.09 32.25 21.5C32.25 21.91 31.91 22.25 31.5 22.25Z" fill="white" />
+                                </svg>
 
-                    </span>
-                    <div className="flex flex-col w-[415px] h-[96px] gap-3">
-                        <span className="w-[305px] h-[36px] text-secondary-900 text-[24px] font-semibold">
-                            Use of multi-card payments
-                        </span>
-                        <span className="w-[415px] h-[48px] text-secondary-400 text-[16px]">
-                            Have more than one debit or credit card? Don't worry, we support payments using more than one card.
-                        </span>
+                            </span>
+                            <div className="flex flex-col w-[415px] h-[96px] gap-3">
+                                <span className="w-full h-[36px] text-secondary-900 text-[24px] font-semibold">
+                                    Clear payment history
+                                </span>
+                                <span className="w-[415px] h-[48px] text-secondary-400 text-[16px]">
+                                    Clear payment history helps you to track your business expenses on specific dates.
+                                </span>
+                            </div>
+                        </div>
+                        <div className="flex w-[497px] h-[96px] gap-8">
+                            <span>
+                                <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect width="50" height="50" rx="12" fill="#7C5CFC" />
+                                    <path d="M28.22 19.55H18.78C18.5 19.55 18.24 19.56 18 19.57C15.63 19.71 15 20.58 15 23.28V23.86C15 24.41 15.45 24.86 16 24.86H31C31.55 24.86 32 24.41 32 23.86V23.28C32 20.3 31.24 19.55 28.22 19.55Z" fill="white" />
+                                    <path d="M16 26.3601C15.45 26.3601 15 26.8101 15 27.3601V30.2701C15 33.2501 15.76 34.0001 18.78 34.0001H28.22C31.19 34.0001 31.97 33.2801 32 30.4301V27.3601C32 26.8101 31.55 26.3601 31 26.3601H16ZM19.96 31.5601H18.25C17.84 31.5601 17.5 31.2201 17.5 30.8101C17.5 30.4001 17.84 30.0601 18.25 30.0601H19.97C20.38 30.0601 20.72 30.4001 20.72 30.8101C20.72 31.2201 20.38 31.5601 19.96 31.5601ZM25.55 31.5601H22.11C21.7 31.5601 21.36 31.2201 21.36 30.8101C21.36 30.4001 21.7 30.0601 22.11 30.0601H25.55C25.96 30.0601 26.3 30.4001 26.3 30.8101C26.3 31.2201 25.97 31.5601 25.55 31.5601Z" fill="white" />
+                                    <path d="M35 26.3301V21.0901C35 17.9601 33.21 16.6001 30.51 16.6001H21.58C20.82 16.6001 20.14 16.7101 19.54 16.9401C19.07 17.1101 18.65 17.3601 18.31 17.6901C18.13 17.8601 18.27 18.1401 18.53 18.1401H29.4C31.65 18.1401 33.47 19.9601 33.47 22.2101V29.3801C33.47 29.6301 33.74 29.7701 33.92 29.5901C34.61 28.8601 35 27.7901 35 26.3301Z" fill="white" />
+                                </svg>
+
+                            </span>
+                            <div className="flex flex-col w-[415px] h-[96px] gap-3">
+                                <span className="w-full h-[36px] text-secondary-900 text-[24px] font-semibold">
+                                    Use of multi-card payments
+                                </span>
+                                <span className="w-[415px] h-[48px] text-secondary-400 text-[16px]">
+                                    Have more than one debit or credit card? Don't worry, we support payments using more than one card.
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-[608px] h-[413px] rounded-[12px]">
+                        <img src={Content} alt="" />
                     </div>
                 </div>
-            </div>
-
-            <div className="w-[608px] h-[413px] absolute top-[370px] left-[712px] rounded-[12px]">
-                <img src={Content} alt="" />
             </div>
         </section>
     );
