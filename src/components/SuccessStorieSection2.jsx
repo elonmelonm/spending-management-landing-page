1df8025b2197a8dc4d6abf5c6968261b6b573fe5
AKAKPO ELONM LUC <elonmlucakakpo@gmail.com>
2025-08-31 05:09:20 +0100

    fix all success stories section style

--- src/components/SuccessStorieSection2.jsx
+++ src/components/SuccessStorieSection2.jsx
@@ -3,65 +3,67 @@ import Success from '../assets/Success Stories (1).png';
 
 const SuccessStorieSection2 = () => {
     return (
-        <section className="w-[1440px] h-[890px] relative bg-white flex flex-col">
-            <div className="flex flex-col w-[634px] h-[162px] absolute justify-center top-[120px] left-[120px] gap-3">
-                <span className="w-[242px] h-[30px] text-primary-500 font-semibold text-[20px]">
-                    INCREASE PRODUCTIVITY
-                </span>
-                <p className="w-[634px] h-[120x] text-[40px] font-bold text-secondary-700">
-                    Reduce Time in Doing Manual Work Managing Expenses
-                </p>
-            </div>
-            <div className="flex flex-row w-[368px] h-[56px] rounded-[30px] absolute top-[346px] left-[120px] gap-4">
-                <button className="w-[169px] h-[52px] bg-secondary-100 text-primary-0 border-none rounded-[30px]">
-                    With Spend.In
-                </button> 
-                <button className="w-[169px] h-[52px] bg-primary-500 text-secondary-300 border-none rounded-[30px]">
-                    With Spend.In
-                </button> 
-            </div>
-
-            <div className="w-[486px] h-[434px] absolute top-[346px] left-[834px] rounded-[12px]">
-                <img src={Success} alt="" />
-            </div>
-
-            <div className="flex flex-col w-[526px] h-[294px] p-8 text-secondary-700 absolute justify-center top-[450px] left-[120px] rounded-[12px] gap-8">
-                <span className="w-[475px] h-[72px] text-secondary-700 font-bold  ">
-                    Taking too long to tidy up administrative files makes it unproductive
-                </span>
-                <div className="w-[526px] h-[54px] text-[18px] flex flex-row gap-4">
-                    <span>
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z" fill="#FF4423"/>
-                        </svg>
+        <section className="w-full h-[890px] p-[120px] items-center relative bg-white flex flex-col">
+            <div className="flex flex-col gap-16">
+                <div className="flex flex-col w-[634px] h-[162px] justify-center gap-3">
+                    <span className="w-full h-[30px] text-primary-500 font-semibold text-[20px]">
+                        INCREASE PRODUCTIVITY
                     </span>
-                    <p className="w-[490px] h-[54px] text-[18px]">
-                        Complex recording process due to every administrative file in a different place.
+                    <p className="w-[634px] h-[120x] text-[40px] font-bold text-secondary-700">
+                        Reduce Time in Doing Manual Work Managing Expenses
                     </p>
                 </div>
-                <div className="w-[526px] h-[54px] text-[18px] flex flex-row gap-4">
-                    <span>
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z" fill="#FF4423"/>
-                        </svg>
-                    </span>
-                    <p className="w-[490px] h-[54px] text-[18px]">
-                        Need more effort to pay manually one by one invoice because there is no payment accommodation.
-                    </p>
-                </div>
-                <div className="w-[526px] h-[54px] text-[18px] flex flex-row gap-4">
-                    <span>
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z" fill="#FF4423"/>
-                        </svg>
-                    </span>
-                    <p className="w-[490px] h-[54px] text-[18px]">
-                        Manual data arranging needs a long time because the different months/years are not in the same place.
-                    </p>
+                <div className="flex flex-row gap-[188px]">
+                    <div className="flex flex-col gap-12">
+                        <div className="flex flex-row w-[368px] h-[56px] rounded-[30px] gap-4">
+                            <button className="w-[169px] h-[52px] bg-secondary-100 text-secondary-300 border-none rounded-[30px]">
+                                With Spend.In
+                            </button>
+                            <button className="w-[169px] h-[52px] bg-primary-500 text-primary-0 border-none rounded-[30px]">
+                                With Spend.In
+                            </button>
+                        </div>
+                        <div className="flex flex-col w-[526px] h-[294px] p-8 text-secondary-700 justify-center rounded-[12px] gap-8">
+                            <span className="w-[475px] h-[36px] text-secondary-700 font-bold  ">
+                                Track Business Expenses until its Milisecond
+                            </span>
+                            <div className="w-[526px] h-[54px] text-[18px] flex flex-row gap-4">
+                                <span>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z" fill="#FF4423" />
+                                    </svg>
+                                </span>
+                                <p className="w-[490px] h-[54px] text-[18px]">
+                                    Complex recording process due to every administrative file in a different place.
+                                </p>
+                            </div>
+                            <div className="w-[526px] h-[54px] text-[18px] flex flex-row gap-4">
+                                <span>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z" fill="#FF4423" />
+                                    </svg>
+                                </span>
+                                <p className="w-[490px] h-[54px] text-[18px]">
+                                    Need more effort to pay manually one by one invoice because there is no payment accommodation.
+                                </p>
+                            </div>
+                            <div className="w-[526px] h-[54px] text-[18px] flex flex-row gap-4">
+                                <span>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z" fill="#FF4423" />
+                                    </svg>
+                                </span>
+                                <p className="w-[490px] h-[54px] text-[18px]">
+                                    Manual data arranging needs a long time because the different months/years are not in the same place.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-[486px] h-[434px] rounded-[12px]">
+                        <img src={Success} alt="" />
+                    </div>
                 </div>
             </div>
-
-            
         </section>
     );
 };
