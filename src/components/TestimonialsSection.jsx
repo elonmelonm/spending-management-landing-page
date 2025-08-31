4e406dc6bc1a1779d72225477a19ffadcdf358c6
AKAKPO ELONM LUC <elonmlucakakpo@gmail.com>
2025-08-31 07:11:33 +0100

    fix testimonials section style

--- src/components/TestimonialsSection.jsx
+++ src/components/TestimonialsSection.jsx
@@ -8,13 +8,13 @@ import Image3 from '../assets/Frame 33700 (2).png';
 
 const TestimonialsSection = () => {
     return (
-        <section className="w-[1440px] h-[890px] relative bg-secondary-700 flex flex-col">
-            <div className="flex flex-col w-[568px] h-[186px] absolute items-center justify-center top-[120px] left-[436px] gap-6">
-                <div className="w-[379px] h-[186px] flex flex-col items-center justify-center  gap-3">
-                    <span className="w-[160px] h-[30px] text-primary-500 font-semibold text-[20px]">
+        <section className="w-full h-[967px] p-[120px] items-center relative bg-secondary-700 flex flex-col gap-16">
+            <div className="flex flex-col w-[568px] h-[186px] items-center justify-center gap-6">
+                <div className="w-[459px] h-[186px] flex flex-col items-center justify-center  gap-3">
+                    <span className="w-full h-[30px] text-center text-primary-500 font-semibold text-[20px]">
                         WHAT THEY SAY
                     </span>
-                    <span className="w-[379px] h-[60px] text-primary-0 font-bold text-[40px] tracking-[-0.03em]">
+                    <span className="w-full h-[60px] text-center text-primary-0 font-bold text-[40px] tracking-[-0.03em]">
                         Our User Kind Words
                     </span>
                 </div>
@@ -22,8 +22,7 @@ const TestimonialsSection = () => {
                     Here are some testimonials from our user after using Spend.In to manage their business  expenses.
                 </div>
             </div>
-
-            <div className="flex flex-row w-[1200px] h-[363px] absolute top-[370px] left-[120px] gap-6">
+            <div className="flex flex-row w-[1200px] h-[363px] gap-6">
                 <div className="flex flex-col w-[384px] p-8 h-[363px] rounded-[12px] bg-secondary-500 gap-8">
                     <span className="w-[320px] h-[27px] text-[18px] font-semibold text-primary-0 pb-4 ">
                         It’s just incredible!
@@ -31,9 +30,7 @@ const TestimonialsSection = () => {
                     <span className="w-[320px] h-[120px] text-[16px] font-medium text-primary-0 pb-[34px] ">
                         It’s just 1 month since I’m using Spend.In to manage my business expenses, but the result is very satisfying! My business finance now more neat than before, thanks to Spend.In!
                     </span>
-
                     <div class="w-[320px] h-0 left-8 top-[229px] opacity-20 border-t border-[#90A3BF]"></div>
-
                     <div className="flex gap-6 items-center">
                         <span>
                             <img src={Image1} alt="" />
@@ -48,7 +45,6 @@ const TestimonialsSection = () => {
                         </div>
                     </div>
                 </div>
-
                 <div className="flex flex-col w-[384px] p-8 h-[363px] rounded-[12px] bg-secondary-500 gap-8">
                     <span className="w-[320px] h-[27px] text-[18px] font-semibold text-primary-0 pb-4 ">
                         Satisfied User Here!
@@ -56,9 +52,7 @@ const TestimonialsSection = () => {
                     <span className="w-[320px] h-[120px] text-[16px] font-medium text-primary-0 pb-[34px] ">
                         Never thought that with Spend.In managing my business expenses is so easy! Been using this platform for 3 months and still counting!
                     </span>
-
                     <div class=" w-[320px] h-0 left-8 top-[229px] opacity-20 border-t border-[#90A3BF]"></div>
-
                     <div className="flex gap-6 items-center">
                         <span>
                             <img src={Image2} alt="" />
@@ -73,7 +67,6 @@ const TestimonialsSection = () => {
                         </div>
                     </div>
                 </div>
-
                 <div className="flex flex-col w-[384px] p-8 h-[363px] rounded-[12px] bg-secondary-500 gap-8">
                     <span className="w-[320px] h-[27px] text-[18px] font-semibold text-primary-0 pb-4 ">
                         No doubt, Spend.In is the best!
@@ -99,25 +92,20 @@ const TestimonialsSection = () => {
                     </div>
                 </div>
             </div>
-
-            
-            <div className="flex flex-row w-[122px] h-[50px]  absolute top-[797px] left-[659px] gap-6">
+            <div className="flex flex-row w-[122px] h-[50px] gap-6">
                 <button>
                     <svg width="48" height="50" viewBox="0 0 48 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="48" height="50" rx="24" fill="#1A202C"/>
-                        <path d="M22.1775 20.4473L17.625 24.9998L22.1775 29.5523" stroke="#C3D4E9" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M30.3749 25H17.7524" stroke="#C3D4E9" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        <rect width="48" height="50" rx="24" fill="#1A202C" />
+                        <path d="M22.1775 20.4473L17.625 24.9998L22.1775 29.5523" stroke="#C3D4E9" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M30.3749 25H17.7524" stroke="#C3D4E9" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
-
-                </button> 
+                </button>
                 <button className="flex w-[50px] h-[50px] bg-primary-500 justify-center items-center rounded-full ">
                     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10.8225 4.44727L15.375 8.99977L10.8225 13.5523" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M2.625 9H15.2475" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M10.8225 4.44727L15.375 8.99977L10.8225 13.5523" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M2.625 9H15.2475" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
-
-
-                </button> 
+                </button>
             </div>
         </section>
     );
